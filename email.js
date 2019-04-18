78da65bcc6382e3b908750e87c6e20c35eb81a94
Malcolm Craney <mjcraney@calpoly.edu>
2019-04-18 12:05:56 -0700

    Email Update

--- email.js
+++ email.js
@@ -1,5 +1,11 @@
-function showAlert() {
-    document.getElementById("inputEmail").value = "";
-    document.getElementById("inputIssue").value = "";
-    alert("Email sent!")
+window.onload = function() {
+    document.getElementById("form").onsubmit = function(e) {
+        e.preventDefault();
+    
+        let form = document.getElementById("form");
+        let subject = document.getElementById("subject").value;
+        let body = document.getElementById("body").value;
+    
+        window.location.assign('mailto:mcraney129@gmail.com?subject='+ subject  + '&body=' + body);
+    }
 }
